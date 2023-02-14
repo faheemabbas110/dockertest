bd83c17bdd7deb62250e01eacb29366a76c157bf
faheemabbas110 <s.faheem.abbas@gmail.com>
2023-02-14 21:24:41 +0100

    v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(){
   return(
     <header className="mx-auto p-8 bg-red-700">
-      <h1 className="text-white text-4xl">!!! Docker Pipline testing -- 2</h1>
+      <h1 className="text-white text-4xl">!!! Docker Pipline testing -- V2</h1>
     </header>
   )
 }
